d104e6c6f90bbd5fd1329f30fe50e2eb3eb0093b
bhavana.extra@gmail.com <bhavana.extra@gmail.com>
2026-07-24 17:08:24 +0000

    commit

--- sdp_bundle/employee/transformations/employee.py
+++ sdp_bundle/employee/transformations/employee.py
@@ -4,7 +4,7 @@ from pyspark.sql.types import *
 
 # Import data from parameters
 emp_id = spark.conf.get("id", "1")
-name = spark.conf.get("name", "default")
+name = spark.conf.get("name", "test")
 
 data = [(emp_id, name, NOW()),]
 schema = ["employee_id","employee_name","load_time"]
