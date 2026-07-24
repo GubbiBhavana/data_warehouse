614c0dc0dbc663a2098a8c1695bb99fe8e4f5448
bhavana.extra@gmail.com <bhavana.extra@gmail.com>
2026-07-24 17:07:50 +0000

    commit changes

--- sdp_bundle/employee/transformations/employee.py
+++ sdp_bundle/employee/transformations/employee.py
@@ -3,8 +3,8 @@ from pyspark import pipelines as dp
 from pyspark.sql.types import *
 
 # Import data from parameters
-emp_id = spark.conf.get("id")
-name = spark.conf.get("name")
+emp_id = spark.conf.get("id", "1")
+name = spark.conf.get("name", "default")
 
 data = [(emp_id, name, NOW()),]
 schema = ["employee_id","employee_name","load_time"]
