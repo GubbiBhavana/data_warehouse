91a569f86df5fde7898606b9ca0926e0344e45ba
bhavana.extra@gmail.com <bhavana.extra@gmail.com>
2026-07-24 17:53:04 +0000

    commit

--- sdp_bundle/employee/transformations/employee.py
+++ sdp_bundle/employee/transformations/employee.py
@@ -1,19 +1,16 @@
 #Import Libraries
 from pyspark import pipelines as dp
 from pyspark.sql.types import *
-
-# Import data from parameters
-emp_id = spark.conf.get("id", "1")
-name = spark.conf.get("name", "test")
-
-data = [(emp_id, name, NOW()),]
-schema = ["employee_id","employee_name","load_time"]
+from pyspark.sql.functions import current_timestamp
 
 @dp.materialized_view(
   comment="Raw data on employee")
+def employee():
+  # Read parameters inside the function so they're evaluated per run
+  emp_id = spark.conf.get("id", "1")
+  name = spark.conf.get("name", "test")
   
-def sales():
-  return spark.createDataFrame(
-    data,
-    schema
-  )
+  data = [(emp_id, name, current_timestamp())]
+  schema = ["employee_id", "employee_name", "load_time"]
+  
+  return spark.createDataFrame(data, schema)
