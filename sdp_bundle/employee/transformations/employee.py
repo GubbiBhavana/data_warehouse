6dfe650a89ef4b1280f06879460ec1c8e21de30b
bhavana.extra@gmail.com <bhavana.extra@gmail.com>
2026-07-24 14:09:21 +0000

    commit

---
+++ sdp_bundle/employee/transformations/employee.py
@@ -0,0 +1,19 @@
+#Import Libraries
+from pyspark import pipelines as dp
+from pyspark.sql.types import *
+
+# Import data from parameters
+emp_id = spark.conf.get("id")
+name = spark.conf.get("name")
+
+data = [(emp_id, name, NOW()),]
+schema = ["employee_id","employee_name","load_time"]
+
+@dp.materialized_view(
+  comment="Raw data on employee")
+  
+def sales():
+  return spark.createDataFrame(
+    data,
+    schema
+  )
